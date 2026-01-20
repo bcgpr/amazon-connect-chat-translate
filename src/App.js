202bf1acdac98f4af50ca1d1b06778cca7cab062
Leonard Molano <lmolano@bridgewatercg.com>
2026-01-20 00:55:47 -0400

    allowUnauthenticatedIdentities for translate apis

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import { Amplify }  from '@aws-amplify/core';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'semantic-ui-less/semantic.less';
 import Ccp from './components/ccp';
-//import { autoSignIn } from '@aws-amplify/auth';
 
 // Component
-function App({ signOut, user }) {
+function App() {
   const [isConfigured, setIsConfigured] = useState(false);
   
   useEffect(() => {
     configureAuth();
-    //signedIn();
   }, []);
 
   const configureAuth = () => {
     Amplify.configure(awsconfig);
     setIsConfigured(true);
   };
-  //const signedIn = async () => {
-    //await autoSignIn();
-  //};
 
   return (
     <div className="App">
-      {isConfigured && <Ccp user={user} signOut={signOut} />}
+      {isConfigured && <Ccp />}
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
